263056da312d49902af557c693ad9d3fd24fb1d1
sugn <sug@gmail.com>
2015-10-21 03:42:31 +0900

    15/10/21 committed

--- database/read_table.py
+++ database/read_table.py
@@ -15,10 +15,19 @@ import xlrd
 def read_table(filename):
 
     book = xlrd.open_workbook(filename)
-    for name in book.sheet_names():
-        print(name)
+    s1 = book.sheet_by_index(0)
+    members = []
 
+    for row in range(1,s1.nrows):
+
+        name = s1.cell(row,0).value
+        grade = s1.cell(row,1).value
+        member = [name, grade]
+        members.append(member)
+
+    return members
 
 if __name__ == '__main__':
 
-    read_table('2015.xls')
+    members = read_table('2015.xls')
+    print members
