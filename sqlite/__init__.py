7f73f559c96d00a524c4506e05a6ba65d01c7d67
sugn <sug@gmail.com>
2015-10-21 01:27:01 +0900

    first commit

---
+++ sqlite/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- 
+# Created on 2015 10 21 
+
+__author__ = 'SugN'
+__copyright__ = "Copyright 2015, SugN All Rights Reserved."
+__credits__ = [""]
+__license__ = "GPL"
+__version__ = "1.0.1"
+
+"""
+
+"""
+
+if __name__ == '__main__':
+    
